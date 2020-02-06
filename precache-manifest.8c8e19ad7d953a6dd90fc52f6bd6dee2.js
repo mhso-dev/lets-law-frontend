72250d3abf7e2172e7361d7c1e6301691e0f36f0
mhso-dev <mhso.developer@gmail.com>
2020-02-06 15:17:45 +0900

    Updates

---
+++ precache-manifest.8c8e19ad7d953a6dd90fc52f6bd6dee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f450a21c33db7ceadb283d4b6972aa6",
+    "url": "/lets-law-frontend/index.html"
+  },
+  {
+    "revision": "8459846bc00a450311af",
+    "url": "/lets-law-frontend/static/css/2.600dfb7d.chunk.css"
+  },
+  {
+    "revision": "a1ad2d5b0f0427d8c28c",
+    "url": "/lets-law-frontend/static/css/main.4e2b52a1.chunk.css"
+  },
+  {
+    "revision": "8459846bc00a450311af",
+    "url": "/lets-law-frontend/static/js/2.3fd9b79a.chunk.js"
+  },
+  {
+    "revision": "a1ad2d5b0f0427d8c28c",
+    "url": "/lets-law-frontend/static/js/main.b03fecd9.chunk.js"
+  },
+  {
+    "revision": "17f5d8b4cdc969f3a1d5",
+    "url": "/lets-law-frontend/static/js/runtime-main.fa5d77b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lets-law-demo/precache-manifest.8feaadc42e7602e77c98fb8711467af9.js"
+  "/lets-law-frontend/precache-manifest.8c8e19ad7d953a6dd90fc52f6bd6dee2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lets-law-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lets-law-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
